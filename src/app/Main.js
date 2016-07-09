4d26459682efb76341d794b47e562490aee7ea9d
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-07-09 18:42:50 +0300

    First minor commit

--- src/app/Main.js
+++ src/app/Main.js
@@ -19,8 +19,10 @@ const styles = {
 
 const muiTheme = getMuiTheme({
   palette: {
-    accent1Color: deepOrange500,
-  },
+    primary1Color: '#607D8B',
+    accent1Color: '#00BCD4',
+    textColor: '#212121',
+  }
 });
 
 class Main extends Component {
@@ -50,32 +52,33 @@ class Main extends Component {
   render() {
     const standardActions = (
       <FlatButton
-        label="Ok"
-        primary={true}
-        onTouchTap={this.handleRequestClose}
+      label="Ok"
+      primary={true}
+      onTouchTap={this.handleRequestClose}
       />
     );
 
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
-        <div style={styles.container}>
-          <Dialog
-            open={this.state.open}
-            title="Super Secret Password"
-            actions={standardActions}
-            onRequestClose={this.handleRequestClose}
-          >
-            1-2-3-4-5
-          </Dialog>
-          <h1>Material-UI</h1>
-          <h2>example project</h2>
-          <RaisedButton
-            label="Super Secret Password"
-            secondary={true}
-            onTouchTap={this.handleTouchTap}
-          />
-        </div>
-      </MuiThemeProvider>
+      <div style={styles.container}>
+      <Dialog
+      open={this.state.open}
+      title="Super Secret Password"
+      actions={standardActions}
+      onRequestClose={this.handleRequestClose}
+      >
+      1-2-3-4-5
+      </Dialog>
+      <h1>Material-UI</h1>
+      <h2>example project</h2>
+      <RaisedButton
+      label="Super Secret Password"
+      secondary={true}
+      backgroundColor="a32fae"
+      onTouchTap={this.handleTouchTap}
+      />
+    </div>
+    </MuiThemeProvider>
     );
   }
 }
