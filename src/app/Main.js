eabaa36672fef2a887b52bbaaab47aca34d86062
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-07-09 18:56:16 +0300

    Blank slate

--- src/app/Main.js
+++ src/app/Main.js
@@ -1,14 +1,13 @@
-/**
- * In this file, we create a React component
- * which incorporates components provided by Material-UI.
- */
-import React, {Component} from 'react';
-import RaisedButton from 'material-ui/RaisedButton';
-import Dialog from 'material-ui/Dialog';
-import {deepOrange500} from 'material-ui/styles/colors';
-import FlatButton from 'material-ui/FlatButton';
+import React from 'react';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: '#607D8B',
+    accent1Color: '#00BCD4',
+  },
+});
 
 const styles = {
   container: {
@@ -17,70 +16,18 @@ const styles = {
   },
 };
 
-const muiTheme = getMuiTheme({
-  palette: {
-    primary1Color: '#607D8B',
-    accent1Color: '#00BCD4',
-    textColor: '#212121',
-  }
-});
-
-class Main extends Component {
-  constructor(props, context) {
-    super(props, context);
-
-    this.handleRequestClose = this.handleRequestClose.bind(this);
-    this.handleTouchTap = this.handleTouchTap.bind(this);
-
-    this.state = {
-      open: false,
-    };
-  }
-
-  handleRequestClose() {
-    this.setState({
-      open: false,
-    });
-  }
-
-  handleTouchTap() {
-    this.setState({
-      open: true,
-    });
-  }
+class Main extends React.Component {
 
   render() {
-    const standardActions = (
-      <FlatButton
-      label="Ok"
-      primary={true}
-      onTouchTap={this.handleRequestClose}
-      />
-    );
-
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
-      <div style={styles.container}>
-      <Dialog
-      open={this.state.open}
-      title="Super Secret Password"
-      actions={standardActions}
-      onRequestClose={this.handleRequestClose}
-      >
-      1-2-3-4-5
-      </Dialog>
-      <h1>Material-UI</h1>
-      <h2>example project</h2>
-      <RaisedButton
-      label="Super Secret Password"
-      secondary={true}
-      backgroundColor="a32fae"
-      onTouchTap={this.handleTouchTap}
-      />
-    </div>
-    </MuiThemeProvider>
+        <div style={styles.container}>
+          <h1>Hello World</h1>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
 
 export default Main;
+
